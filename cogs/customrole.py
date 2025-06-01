7da45ec8f3652d7a0d7b107bb8198b1987b54548
hakuuu9 <hakuunlyy@gmail.com>
2025-06-01 19:34:55 +0800

    Update customrole.py

--- cogs/customrole.py
+++ cogs/customrole.py
@@ -12,8 +12,8 @@ class CustomRole(commands.Cog):
         self.trio_role = {879936602414133288, 1275065396705362041, 1092795368556732478}
         self.check_expiry.start()
 
-    def is_staff(self, member: discord.Member):
-        return any(role.id in self.trio_role for role in member.roles)
+   def is_staff(self, user: discord.User | discord.Member):
+    return user.id in self.trio_role
 
     @app_commands.command(name="role-list", description="Register a custom role for a member (staff only)")
     @app_commands.describe(member="Member to assign custom role", role_name="Name of the custom role")
