9a82bb335c54e9d5f575b3e8401f9a7966cc51ec
hakuuu9 <hakuunlyy@gmail.com>
2025-06-24 22:22:35 +0800

    Update shop.py

--- cogs/shop.py
+++ cogs/shop.py
@@ -17,7 +17,7 @@ CUSTOM_ROLE_COST = 150000
 # Role shop items
 ROLE_COST = 30000
 ROLE_ITEMS = {
-    "moss sprite": "<:mosssprite:1379352174718619738>",
+    "sootkeeper": "<:Sootkeeper:1387075130424819896>",
     "enigma": "<:Enigma:1379352300300013618>",
     "elderleaf": "<:elderleaf:1379352378771509268>",
     "solace": "<:solace:1379352464624713758>",
